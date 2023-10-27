d69c84d261ac7975080b8021ddb828b02ba9d0e6
All_local <All_local@510-004>
2023-10-27 11:26:03 +0400

    hfghfgh#

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -39,11 +39,28 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.buttonShow1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxGroupDel = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxFamiliaDel = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNameDel = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxOtchestvoDel = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxPolDel = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.buttonShow2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonInsert
             // 
-            this.buttonInsert.Location = new System.Drawing.Point(327, 178);
+            this.buttonInsert.Location = new System.Drawing.Point(284, 130);
             this.buttonInsert.Name = "buttonInsert";
             this.buttonInsert.Size = new System.Drawing.Size(130, 23);
             this.buttonInsert.TabIndex = 0;
@@ -53,35 +70,35 @@
             // 
             // textBoxFamilia
             // 
-            this.textBoxFamilia.Location = new System.Drawing.Point(56, 88);
+            this.textBoxFamilia.Location = new System.Drawing.Point(13, 71);
             this.textBoxFamilia.Name = "textBoxFamilia";
             this.textBoxFamilia.Size = new System.Drawing.Size(100, 20);
             this.textBoxFamilia.TabIndex = 1;
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(214, 88);
+            this.textBoxName.Location = new System.Drawing.Point(171, 71);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(100, 20);
             this.textBoxName.TabIndex = 2;
             // 
             // textBoxOtchestvo
             // 
-            this.textBoxOtchestvo.Location = new System.Drawing.Point(369, 88);
+            this.textBoxOtchestvo.Location = new System.Drawing.Point(326, 71);
             this.textBoxOtchestvo.Name = "textBoxOtchestvo";
             this.textBoxOtchestvo.Size = new System.Drawing.Size(100, 20);
             this.textBoxOtchestvo.TabIndex = 3;
             // 
             // textBoxGroup
             // 
-            this.textBoxGroup.Location = new System.Drawing.Point(525, 88);
+            this.textBoxGroup.Location = new System.Drawing.Point(482, 71);
             this.textBoxGroup.Name = "textBoxGroup";
             this.textBoxGroup.Size = new System.Drawing.Size(100, 20);
             this.textBoxGroup.TabIndex = 4;
             // 
             // textBoxPol
             // 
-            this.textBoxPol.Location = new System.Drawing.Point(674, 88);
+            this.textBoxPol.Location = new System.Drawing.Point(631, 71);
             this.textBoxPol.Name = "textBoxPol";
             this.textBoxPol.Size = new System.Drawing.Size(100, 20);
             this.textBoxPol.TabIndex = 5;
@@ -89,7 +106,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(93, 56);
+            this.label1.Location = new System.Drawing.Point(50, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 6;
@@ -98,7 +115,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(255, 64);
+            this.label2.Location = new System.Drawing.Point(212, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 7;
@@ -107,7 +124,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(403, 64);
+            this.label3.Location = new System.Drawing.Point(360, 47);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 8;
@@ -116,7 +133,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(556, 64);
+            this.label4.Location = new System.Drawing.Point(513, 47);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 13);
             this.label4.TabIndex = 9;
@@ -125,32 +142,180 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(705, 64);
+            this.label5.Location = new System.Drawing.Point(662, 47);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(27, 13);
             this.label5.TabIndex = 10;
             this.label5.Text = "Пол";
             // 
+            // buttonShow1
+            // 
+            this.buttonShow1.Location = new System.Drawing.Point(12, 12);
+            this.buttonShow1.Name = "buttonShow1";
+            this.buttonShow1.Size = new System.Drawing.Size(181, 23);
+            this.buttonShow1.TabIndex = 11;
+            this.buttonShow1.Text = "Запрос на добавление";
+            this.buttonShow1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonShow1.UseVisualStyleBackColor = true;
+            this.buttonShow1.Click += new System.EventHandler(this.buttonShow1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxGroup);
+            this.groupBox1.Controls.Add(this.buttonInsert);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxFamilia);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxOtchestvo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxPol);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(760, 229);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxGroupDel);
+            this.groupBox2.Controls.Add(this.buttonDelete);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBoxFamiliaDel);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBoxNameDel);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBoxOtchestvoDel);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.textBoxPolDel);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(12, 305);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(760, 229);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // textBoxGroupDel
+            // 
+            this.textBoxGroupDel.Location = new System.Drawing.Point(482, 71);
+            this.textBoxGroupDel.Name = "textBoxGroupDel";
+            this.textBoxGroupDel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGroupDel.TabIndex = 4;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(284, 130);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(130, 23);
+            this.buttonDelete.TabIndex = 0;
+            this.buttonDelete.Text = "Удалить студента";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(662, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Пол";
+            // 
+            // textBoxFamiliaDel
+            // 
+            this.textBoxFamiliaDel.Location = new System.Drawing.Point(13, 71);
+            this.textBoxFamiliaDel.Name = "textBoxFamiliaDel";
+            this.textBoxFamiliaDel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFamiliaDel.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(513, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Группа";
+            // 
+            // textBoxNameDel
+            // 
+            this.textBoxNameDel.Location = new System.Drawing.Point(171, 71);
+            this.textBoxNameDel.Name = "textBoxNameDel";
+            this.textBoxNameDel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNameDel.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(360, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Отчество";
+            // 
+            // textBoxOtchestvoDel
+            // 
+            this.textBoxOtchestvoDel.Location = new System.Drawing.Point(326, 71);
+            this.textBoxOtchestvoDel.Name = "textBoxOtchestvoDel";
+            this.textBoxOtchestvoDel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOtchestvoDel.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(212, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Имя";
+            // 
+            // textBoxPolDel
+            // 
+            this.textBoxPolDel.Location = new System.Drawing.Point(631, 71);
+            this.textBoxPolDel.Name = "textBoxPolDel";
+            this.textBoxPolDel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPolDel.TabIndex = 5;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(50, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Фамилия";
+            // 
+            // buttonShow2
+            // 
+            this.buttonShow2.Location = new System.Drawing.Point(12, 276);
+            this.buttonShow2.Name = "buttonShow2";
+            this.buttonShow2.Size = new System.Drawing.Size(181, 23);
+            this.buttonShow2.TabIndex = 13;
+            this.buttonShow2.Text = "Запрос на удаление";
+            this.buttonShow2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonShow2.UseVisualStyleBackColor = true;
+            this.buttonShow2.Click += new System.EventHandler(this.buttonShow2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxPol);
-            this.Controls.Add(this.textBoxGroup);
-            this.Controls.Add(this.textBoxOtchestvo);
-            this.Controls.Add(this.textBoxName);
-            this.Controls.Add(this.textBoxFamilia);
-            this.Controls.Add(this.buttonInsert);
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonShow2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonShow1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -167,6 +332,21 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonShow1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxGroupDel;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxFamiliaDel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNameDel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxOtchestvoDel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxPolDel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button buttonShow2;
     }
 }
 
