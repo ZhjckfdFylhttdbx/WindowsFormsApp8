47f2c87abf44bfa80b198a9616206e8b9e1d79c0
All_local <All_local@510-004>
2023-10-27 11:26:10 +0400

    ыфв

--- WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/Form1.cs
@@ -34,5 +34,48 @@ namespace WindowsFormsApp8
             sqlDataAdapter.Fill(dataTable);
             MessageBox.Show("Успешное заполнение!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        private void buttonShow1_Click(object sender, EventArgs e)
+        {
+            if (groupBox1.Visible == false)
+            {
+                groupBox1.Visible = true;
+            }
+            else groupBox1.Visible = false;
+        }
+
+        private void buttonShow2_Click(object sender, EventArgs e)
+        {
+            if (groupBox2.Visible == false)
+            {
+                groupBox2.Visible = true;
+            }
+            else groupBox2.Visible = false;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            Database database1 = new Database();
+            SqlDataAdapter sqlDataAdapter1 = new SqlDataAdapter();
+            DataTable dataTable1 = new DataTable();
+            string loginquarry1 = $"select count(*) from [dbo].[Table] where [Фамилия] = " + textBoxFamiliaDel.Text + ",[Имя] =  " + textBoxNameDel.Text + ",[Отчество] = " + textBoxOtchestvoDel.Text + ",[Группа] = " + textBoxGroupDel.Text + ",[Пол] = " + textBoxPolDel.Text + "";
+            SqlCommand logincommand1 = new SqlCommand(loginquarry1, database1.GetConnection());
+            sqlDataAdapter1.SelectCommand = logincommand1;
+            sqlDataAdapter1.Fill(dataTable1);
+            MessageBox.Show("Успешноеудаление!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (Convert.ToInt32(dataTable1.Rows[0][0].ToString()) ==1)
+            {
+            Database database = new Database();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+            DataTable dataTable = new DataTable();
+            string loginquarry = $"delete from [dbo].[Table] where [Фамилия] = " + textBoxFamiliaDel.Text + ",[Имя] = " + textBoxNameDel.Text + ",[Отчество] = " + textBoxOtchestvoDel.Text + ",[Группа] = " + textBoxGroupDel.Text + ",[Пол] = " + textBoxPolDel.Text + "";
+            SqlCommand logincommand = new SqlCommand(loginquarry, database.GetConnection());
+            sqlDataAdapter.SelectCommand = logincommand;
+            sqlDataAdapter.Fill(dataTable);
+            MessageBox.Show("Успешноеудаление!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else MessageBox.Show("Данный студент не найден!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
     }
 }
